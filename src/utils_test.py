66e601ee4160d1f98f843e7098769780b4132277
Chris Reddington <138114508+chrisreddingt0n@users.noreply.github.com>
2024-04-19 11:19:16 +0100

    Add unit tests for URL validation methods

--- src/utils_test.py
+++ src/utils_test.py
@@ -11,5 +11,15 @@ class TestValidationMethods(unittest.TestCase):
         self.assertTrue(utils.is_phone_valid('4255552368'))
         self.assertFalse(utils.is_phone_valid('invalid_string'))
 
+    def test_is_secure_url_valid(self):
+        self.assertTrue(utils.is_secure_url_valid('https://example.com'))
+        self.assertFalse(utils.is_secure_url_valid('http://example.com'))
+        self.assertFalse(utils.is_secure_url_valid('invalid_string'))
+
+    def test_is_github_url_valid(self):
+        self.assertTrue(utils.is_github_url_valid('https://github.com/octocademy/utils-library'))
+        self.assertFalse(utils.is_github_url_valid('https://example.com'))
+        self.assertFalse(utils.is_github_url_valid('invalid_string'))
+
 if __name__ == '__main__':
     unittest.main()
